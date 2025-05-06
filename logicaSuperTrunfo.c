76f19cd8ced9f129d816be872bf4fcb1b0cef245
Gudxy <aalvernazma18@gmail.com>
2025-05-06 14:31:14 +0000

    if_else

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,137 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    
+    char nome[50];
+    int CDC, PT;
+    float area, PIB;
+    unsigned long int populacao;
+
+    printf("== Carta A01 ==\n");
+
+    printf("Nome da cidade: ");
+    scanf("%s", nome);
+
+    printf("Codigo da cidade: ");
+    scanf("%d", &CDC);
+
+    printf("Pontos Turisticos: ");
+    scanf("%d", &PT);
+
+    printf("Populacao: ");
+    scanf("%u", &populacao);
+
+    printf("Area da cidade em KM²: ");
+    scanf("%f", &area);
+
+    printf("PIB da cidade: ");
+    scanf("%f", &PIB);
+
+    float Densidade_Populacional = populacao / area;
+    float PIB_per_capita = PIB / populacao;
+
+    printf("\n-- Informações da Cidade A01 --\n");
+    printf("Nome: %s\n", nome);
+    printf("Código: %d\n", CDC);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f KM²\n", area);
+    printf("PIB: $%.2f\n", PIB);
+    printf("Pontos Turísticos: %d\n", PT);
+    printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional);
+    printf("PIB per Capita: %.2f Reais\n",PIB_per_capita);
+
+    // Segunda cidade
+    char nome2[50];
+    int CDC2, PT2;
+    float area2, PIB2;
+    unsigned long int populacao2;
+
+    printf("\n-- Carta B02 --\n");
+
+    printf("Nome da segunda cidade: ");
+    scanf("%s", nome2);
+
+    printf("Codigo da segunda cidade: ");
+    scanf("%d", &CDC2);
+
+    printf("Pontos Turisticos: ");
+    scanf("%d", &PT2);
+
+    printf("Populacao: ");
+    scanf("%u", &populacao2);
+
+    printf("Area da segunda cidade em KM²: ");
+    scanf("%f", &area2);
+
+    printf("PIB da segunda cidade: ");
+    scanf("%f", &PIB2);
+
+    float Densidade_Populacional2 = populacao2 / area2;
+    float PIB_per_capita2 = PIB2 / populacao2;
+    
+    printf("\n-- Informações da Cidade B02 --\n");
+    printf("Nome: %s\n", nome2);
+    printf("Código: %d\n", CDC2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f KM²\n", area2);
+    printf("PIB: $%.2f\n", PIB2);
+    printf("Pontos Turísticos: %d\n", PT2);
+    printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional2);
+    printf("PIB per Capita: %.2f Reais\n",PIB_per_capita2);
+    
+    long double Super_Poder1 = (float)(populacao + area + PIB + PT + PIB_per_capita + Densidade_Populacional);
+    long double Super_Poder2 = (float)(populacao2 + area2 + PIB2 + PT2 + PIB_per_capita2 + Densidade_Populacional2);
+
+    printf("\n-- Comparação de Cartas --\n");
+
+    printf("População: Carta 1 venceu (%d)\n", populacao > populacao2);
+    printf("Área: Carta 1 venceu (%d)\n", area > area2);
+    printf("PIB: Carta 1 venceu (%d)\n", PIB > PIB2);
+    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", PT > PT2);
+    printf("Densidade Populacional: Carta 2 venceu (%d)\n", Densidade_Populacional > Densidade_Populacional2);
+    printf("PIB per Capita: Carta 1 venceu (%d)\n", PIB_per_capita > PIB_per_capita2);
+    printf("Super Poder: Carta 1 venceu (%.2f)\n", Super_Poder1 > Super_Poder2);
+    
+    
+    printf("\n-- Vencedores entre as duas cartas --\n");
+
+    if (populacao > populacao2) {
+        printf("Carta 1 venceu em populacao!\n");
+    } else if  (populacao < populacao2) {
+        printf("Carta 2 venceu em populacao!\n");
+    }
+
+    if (area > area2) {
+        printf("Carta 1 venceu em area (km2)!\n");
+    } else if (area < area2) {
+        printf("Carta 2 venceu em area (km2)!\n");
+    }
+
+    if (PIB > PIB2) {
+        printf("Carta 1 venceu em PIB!\n");
+    } else if (PIB < PIB2) {
+        printf("Carta 2 venceu em PIB!\n");
+    }
+
+    if (PT > PT2) {
+        printf("Carta 1 venceu em Pontos Turisticos!\n");
+    } else if (PT < PT2) {
+        printf("Carta 2 venceu em Pontos Turisticos!\n");
+    }
+
+    if (Densidade_Populacional > Densidade_Populacional2) {
+        printf("Carta 2 venceu em Densidade Populacional!\n");
+    } else if (Densidade_Populacional < Densidade_Populacional2) {
+        printf("Carta 1 venceu em Densidade populacional!\n");
+    }
+    
+    if (PIB_per_capita > PIB_per_capita2) {
+        printf("Carta 1 venceu em PIB Per Capita!\n");
+    } else if (PIB_per_capita < PIB_per_capita2) {
+        printf("Carta 2 venceu em PIB Per Capita!\n");
+    }
+    
+    
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
